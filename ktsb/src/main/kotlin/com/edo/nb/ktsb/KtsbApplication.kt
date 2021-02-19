5ba726308f671b97e95d55f60db1a26d74f44e6b
Edo <edin.avdic29@gmail.com>
2021-02-20 00:21:45 +0100

    .

--- ktsb/src/main/kotlin/com/edo/nb/ktsb/KtsbApplication.kt
+++ ktsb/src/main/kotlin/com/edo/nb/ktsb/KtsbApplication.kt
@@ -2,6 +2,10 @@ package com.edo.nb.ktsb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class KtsbApplication
@@ -9,3 +13,20 @@ class KtsbApplication
 fun main(args: Array<String>) {
 	runApplication<KtsbApplication>(*args)
 }
+
+@RestController
+class RestController {
+
+	@GetMapping("/stocks/{symbol}")
+	fun prices(@PathVariable symbol: String) : StockPrice {
+		return StockPrice("222", 22.2, LocalDateTime.now())
+	}
+
+	private fun randomStockPrice(): Double {
+		return 0.0
+	}
+}
+
+data class StockPrice(val symbol: String,
+					  val price: Double,
+					  val time: LocalDateTime)
